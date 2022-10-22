16c411a3515096bbc8dfac3b8978cb05400a6892
Aaron Sanchez <aaronsanchezsallan@gmail.com>
2022-10-23 00:23:14 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {Routes, Route, HashRouter, BrowserRouter} from 'react-router-dom';
 import ParticlesBg from "particles-bg";
 import './App.css';
 import Node from './components/Node';
@@ -19,11 +19,13 @@ function App() {
           {/* <ParticlesBg color="#ff0000" num={2} type="polygon" bg={true}/> */}
         </div>
         <div className="App-content">
-          <BrowserRouter>
+          <HashRouter>
+            <Routes>
             <Route exact path="/" component={props => <Node {...props} />}></Route>
             <Route path="/node-list" component={props => <NodeListTeaser {...props} />}></Route>
             <Route path="/node/:id" component={props => <Node {...props} />}></Route>
-          </BrowserRouter>
+            </Routes>
+          </HashRouter>
         </div>
       </div>
     </div>
