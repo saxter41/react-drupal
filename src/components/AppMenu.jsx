88a5b9aa8ddc736fdefd67676f2c3a45d0b9cd93
Aaron Sanchez <aaronsanchezsallan@gmail.com>
2022-10-23 00:33:06 +0200

    init

--- src/components/AppMenu.jsx
+++ src/components/AppMenu.jsx
@@ -4,8 +4,8 @@ import '../assets/css/AppMenu.css';
 const AppMenu = () => {
   return (
     <div className="App-menu">
-        <h3><a href="/react-drupal/node-list">Node list</a></h3>
-        <h3><a href="/react-drupal/node/4070928a-a9a8-4e75-9886-96c80a914a95">Info</a></h3>
+        <h3><a href="/node-list">Node list</a></h3>
+        <h3><a href="/node/4070928a-a9a8-4e75-9886-96c80a914a95">Info</a></h3>
       </div>
   );
 };
